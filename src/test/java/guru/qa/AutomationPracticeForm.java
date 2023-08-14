8a0f9faf8efa0c25c8276a0f7851d4be18718fa6
dmitry mumolin <dmitrbayan5@yandex.ru>
2023-08-15 01:01:17 +0300

    Оптимизация после разбора ДЗ.

--- src/test/java/guru/qa/AutomationPracticeForm.java
+++ src/test/java/guru/qa/AutomationPracticeForm.java
@@ -4,6 +4,7 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Condition.appear;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
@@ -26,22 +27,27 @@ public class AutomationPracticeForm {
         $("#firstName").setValue("Jack");
         $("#lastName").setValue("The Sparrow");
         $("#userEmail").setValue("jack_the_sparrow@gmail.com");
-        $("#genterWrapper").$(byText("Male")).click();
+
+//        $("#genterWrapper").$(byText("Male")).click();
+        $("#gender-radio-1").parent().click(); // вариант наставника
+//        $("label[for=gender-radio-1]").click(); // вариант наставника
+
+
         $("#userNumber").setValue("9876543210");
         $("#userNumber").pressTab();
 
 //       Дата рождения
         $(".react-datepicker__year-select").selectOption("1997");
+//        $(".react-datepicker__year-select").selectOptionByValue("6");
         $(".react-datepicker__month-select").selectOption("March");
-        $(".react-datepicker__day--002:nth-child(1)").click();
+//        $(".react-datepicker__day--002:nth-child(1)").click();
+        $(".react-datepicker__day--028:not(.react-datepicker__day--outside-month)").click();
 
 //        Предметы
-        $("#subjectsInput").setValue("Computer");
-        $("#subjectsWrapper").$(byText("Computer Science")).click();
-        $("#subjectsInput").setValue("Hin");
-        $("#subjectsWrapper").$(byText("Hindi")).click();
-        $("#subjectsInput").setValue("Ph");
-        $("#subjectsWrapper").$(byText("Physics")).click();
+        $("#subjectsInput").setValue("Computer").pressEnter();
+        $("#subjectsInput").setValue("Hin").pressEnter();
+        $("#subjectsInput").setValue("Ph").pressEnter();
+
 
 //        Хобби
         $("#hobbiesWrapper").$(byText("Sports")).click();
@@ -60,11 +66,14 @@ public class AutomationPracticeForm {
         $("#submit").click();
 
 //        Проверки
+
+        $(".modal-dialog").should(appear);
+
         $(".table-responsive").$(byText("Student Name")).parent().shouldHave(text("Jack The Sparrow"));
         $(".table-responsive").$(byText("Student Email")).parent().shouldHave(text("jack_the_sparrow@gmail.com"));
         $(".table-responsive").$(byText("Gender")).parent().shouldHave(text("Male"));
         $(".table-responsive").$(byText("Mobile")).parent().shouldHave(text("9876543210"));
-        $(".table-responsive").$(byText("Date of Birth")).parent().shouldHave(text("02 March,1997"));
+        $(".table-responsive").$(byText("Date of Birth")).parent().shouldHave(text("28 March,1997"));
         $(".table-responsive").$(byText("Subjects")).parent().shouldHave(text("Computer Science, Hindi, Physics"));
         $(".table-responsive").$(byText("Hobbies")).parent().shouldHave(text("Sports, Music"));
         $(".table-responsive").$(byText("Picture")).parent().shouldHave(text("catinabox-150x150.jpg"));
